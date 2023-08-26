c24c28b72e56b38c9e0e4aa1c2e974222fdfa28d
Casey <leptone55@gmail.com>
2023-08-26 17:07:55 +0100

    working prettierd working

--- nvim/lua/prettierd.lua
+++ nvim/lua/prettierd.lua
@@ -1,7 +1,19 @@
 local M = {}
 
 M.format = function()
-  vim.cmd('silent %!cat % | prettierd %')
+    local current_file = vim.fn.expand('%:p')
+
+    -- Save the buffer to disk.
+    vim.cmd('write')
+    
+    -- Format the file on disk using prettierd.
+    local result = vim.fn.system('prettierd ' .. current_file, vim.api.nvim_buf_get_lines(0, 0, -1, false))
+    
+    if vim.v.shell_error == 0 then
+        vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.fn.split(result, '\n'))
+    else
+        print("Error formatting with prettierd")
+    end
 end
 
 vim.cmd([[
