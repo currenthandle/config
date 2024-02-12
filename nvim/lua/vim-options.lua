9d31de4371fcaec7b7b08b5f5a8f69b648b7bf26
Casey <leptone55@gmail.com>
2024-02-12 08:47:32 -0800

    Don't new lines

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -3,8 +3,16 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-vim.keymap.set('i', 'jj', '<ESC>')
-vim.keymap.set('i', 'kk', '<ESC>')
+vim.keymap.set("i", "jj", "<ESC>")
+vim.keymap.set("i", "kk", "<ESC>")
+-- vim.opt_local.formatoptions:remove({ "r", "o" })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ 'r', 'o' })
+  end,
+})
 
 --[[ -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
