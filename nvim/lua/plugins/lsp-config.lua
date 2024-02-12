33ca63c5a91a87487d640a94b581602ab48866b0
Casey <leptone55@gmail.com>
2024-02-12 11:24:23 -0800

    code action in visual mode

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -69,7 +69,9 @@ return {
 			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			-- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			-- vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
 
 			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
